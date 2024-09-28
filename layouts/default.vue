8f26b1572de3039c1a6aabb52731277a6377d63f
Ващенко Дмитрий <divaschenko@basistech.ru>
2024-09-28 13:34:42 +0300

    312

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    useHead({
+        script: [{src:'https://telegram.org/js/telegram-web-app.js', defer: true}],
+    })
+</script>
+
+<template>
+    <header>
+        <h2>
+            Манчкин
+        </h2>
+    </header>
+        <ClientOnly>
+            <slot>
+
+            </slot>
+        </ClientOnly>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div >
+        <h1>Munchkin App</h1>
+    </div>
+</template>
